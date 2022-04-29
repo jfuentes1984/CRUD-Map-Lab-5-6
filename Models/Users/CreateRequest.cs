3b647f9a3418e9d43af1c496c5fc2609893bb770
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-04-29 11:52:13 -0400

    CRUD added

---
+++ Models/Users/CreateRequest.cs
@@ -0,0 +1,36 @@
+namespace WebApi.Models.Users;
+
+using System.ComponentModel.DataAnnotations;
+using WebApi.Entities;
+
+public class CreateRequest
+{
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    public string Address { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [EnumDataType(typeof(Role))]
+    public string Role { get; set; }
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; }
+
+    [Required]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+}
